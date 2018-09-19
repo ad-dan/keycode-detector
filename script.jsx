4bb8f20a3a656cc7fac49f0c193c02e10ec517b1
Ad <adunreal@gmail.com>
2018-09-19 17:32:55 +0530

    Changed gradient

--- script.jsx
+++ script.jsx
@@ -1,56 +1,47 @@
-const KeyCodeDisplay = ({text}) => {
-  return (
-    <div id='key-code'>
-      {text}
-    </div>
-  );
+const KeyCodeDisplay = ({ text }) => {
+  return <div id="key-code">{text}</div>
 }
-const KeyDisplay = ({text}) => {
-  return (
-    <div id='key-text'>
-      {text}
-    </div>
-  )
+const KeyDisplay = ({ text }) => {
+  return <div id="key-text">{text}</div>
 }
 const Button = () => {
   return (
-    <div className='btn' data-clipboard-target='#key-code'>
+    <div className="btn" data-clipboard-target="#key-code">
       Copy to clipboard
     </div>
   )
 }
-class App extends React.Component{
-  constructor(){
-    super();
+class App extends React.Component {
+  constructor() {
+    super()
     this.state = {
-      keyCodepressed: '?',
-      keypressed: '?'
+      keyCodepressed: "?",
+      keypressed: "?"
     }
-    this.handlePress = this.handlePress.bind(this);
+    this.handlePress = this.handlePress.bind(this)
   }
-  handlePress(e){
+  handlePress(e) {
     this.setState({
       keyCodepressed: e.keyCode,
       keypressed: e.key
     })
   }
-  componentDidMount(){
-    window.addEventListener('keypress', this.handlePress);
+  componentDidMount() {
+    window.addEventListener("keypress", this.handlePress)
   }
   render() {
-
-        return (
-      <div id="app" onKeyPress={(e)=>this.handlePress(e)}>
-        <KeyCodeDisplay text={this.state.keyCodepressed}/>
-        <Button />  
+    return (
+      <div id="app" onKeyPress={e => this.handlePress(e)}>
+        <KeyCodeDisplay text={this.state.keyCodepressed} />
+        <Button />
         <KeyDisplay text={this.state.keypressed} />
       </div>
     )
   }
 }
-new Clipboard('.btn', {
-    text: function(trigger) {
-        return document.getElementById('key-code').innerHTML
-    }
-});
-ReactDOM.render(<App/>, document.getElementById('root'));
+new Clipboard(".btn", {
+  text: function(trigger) {
+    return document.getElementById("key-code").innerHTML
+  }
+})
+ReactDOM.render(<App />, document.getElementById("root"))
